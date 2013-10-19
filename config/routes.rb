824a6f6756e6f793f1af94eb63d71a6959d0c3f2
Taylor Smears <tsmears17@gmail.com>
2013-10-19 17:19:55 +0200

    cusomtized forms

--- config/routes.rb
+++ config/routes.rb
@@ -55,7 +55,8 @@ Treehome::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  
+  root :to => 'statuses#index'
 
   # See how all your routes lay out with "rake routes"
 
